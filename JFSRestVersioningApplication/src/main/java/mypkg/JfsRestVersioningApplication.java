92eb42b67826ddf736556f57f5adb3c13e39b151
surendarp2022 <102714180+surendarp2022@users.noreply.github.com>
2022-03-31 00:29:38 +0530

    restversoiningapp

---
+++ JFSRestVersioningApplication/src/main/java/mypkg/JfsRestVersioningApplication.java
@@ -0,0 +1,13 @@
+package mypkg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfsRestVersioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JfsRestVersioningApplication.class, args);
+	}
+
+}
